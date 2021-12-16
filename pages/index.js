fd931f6f89afb1bf481d9532c3e5ebb65194c66d
Kelly Douglass <kdouglass@gmail.com>
2021-12-16 11:26:00 -0600

    add products

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,38 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Grid, Card, CardActionArea, CardMedia, CardContent, Typography, CardActions, Button } from '@material-ui/core'
 import Layout from '../components/Layout'
-import styles from '../styles/Home.module.css'
+import data from '../utils/data'
+
 
 export default function Home() {
   return (
     <Layout>
-    <div>
-      <h1>Products</h1>
-      <ul>Product 1</ul>
-      <ul>Product 1</ul>
-      <ul>Product 1</ul>
-    </div>
+      <div>
+        <h1>Products</h1>
+        <Grid container spacing={3}>
+          {data.products.map((product) => {
+            return (
+              <Grid item md={4} key={product.name}>
+                <Card>
+                  <CardActionArea>
+                    <CardMedia component="img" image={product.image} title={product.name} />
+                    <CardContent>
+                      <Typography>
+                        {product.name}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Typography>${product.price}</Typography>
+                    <Button size="small" color="primary">
+                      Add to Card
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            )
+          })}
+        </Grid>
+      </div>
     </Layout>
   )
 }
